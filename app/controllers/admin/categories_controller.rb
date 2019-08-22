4a280091d5c2e1d8cd96e707cd941d592b5e24aa
Kendall Rowe <kendall.rowe312@gmail.com>
2019-08-22 12:40:47 +0000

    Added link in top nav to admin categories index and removed delete button from page

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,9 +3,6 @@ class Admin::CategoriesController < ApplicationController
 
   def index
     @categories = Category.all
-    # @categories.each do |c|
-    #   puts "Cat name: #{c.name}"
-    # end
   end
 
   def new
