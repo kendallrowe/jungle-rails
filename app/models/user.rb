d141e0c8378b817b52c62e629d152f6ad1f7a01e
Kendall Rowe <kendall.rowe312@gmail.com>
2019-08-26 23:40:42 +0000

    Created base validation for happy bath user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  validates :email, uniqueness: { case_sensitive: false}
+
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  describe "Validations" do
+    it "is valid with valid attributes" do
+      expect(User.create(first_name: "Kendall",
+        last_name: "Rowe",
+        email: "kendall.rowe312@gmail.com",
+        password: "1234",
+        password_confirmation: "1234"
+      )).to be_valid
+    end
+    
+    # context "passwords" do
+    #   it "is valid with valid attributes" do
+    #     expect(cat1.products.new(name: "Kendall",
+    #     description: "hihi",
+    #     price: 10,
+    #     quantity: 3,
+    #   )).to be_valid
+    #   end
+    # end
+
+    # it "is not valid without a name" do
+    #   expect(cat1.products.create(name: nil,
+    #     description: "hihi",
+    #     price: 10,
+    #     quantity: 3,
+    #   ).errors.full_messages[0]).to eq "Name can't be blank"      
+    # end
+
+    # it "is not valid without a price" do
+    #   expect(cat1.products.create(name: "Kendall",
+    #     description: "hihi",
+    #     price: nil,
+    #     quantity: 3,
+    #   ).errors.full_messages[0]).to eq "Price cents is not a number"      
+    # end
+
+    # it "is not valid without a quantity" do
+    #   expect(cat1.products.create(name: "Kendall",
+    #     description: "hihi",
+    #     price: 10,
+    #     quantity: nil,
+    #   ).errors.full_messages[0]).to eq "Quantity can't be blank"      
+    # end
+
+    # it "is not valid without a category" do
+    #   expect(Product.create(name: "Kendall",
+    #     description: "hihi",
+    #     price: 10,
+    #     quantity: 3,
+    #   ).errors.full_messages[0]).to eq "Category can't be blank"      
+    # end
+
+  end
+end
