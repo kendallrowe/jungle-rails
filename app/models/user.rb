185581b046645c72ce0c9adeb7f5056a5d4a4f64
Kendall Rowe <kendall.rowe312@gmail.com>
2019-08-27 01:09:33 +0000

    Added user authenticate with credentials to user class and specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,9 @@ class User < ActiveRecord::Base
   validates :password, presence: true
   validates :password_confirmation, presence: true
 
+  def self.authenticate_with_credentials(email, password)
+    @user = User.find_by(email: email).try(:authenticate, password) 
+    @user ? @user : nil
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -64,4 +64,37 @@ RSpec.describe User, type: :model do
     end
 
   end
+
+  describe ".authenticate_with_credentials" do
+    subject { described_class.new(first_name: "Kendall",
+      last_name: "Rowe",
+      email: "kendall.rowe312@gmail.com",
+      password: "123456",
+      password_confirmation: "123456"
+    )}  
+
+    it "is valid with correct email and password" do
+      subject.save
+      expect(User.authenticate_with_credentials(
+        "kendall.rowe312@gmail.com",
+        "123456" 
+      )).to_not be_nil
+    end
+
+    it "is not valid with incorrect email and correct password" do
+      subject.save
+      expect(User.authenticate_with_credentials(
+        "kendall.rowe312@gmail.co",
+        "123456" 
+      )).to be_nil
+    end
+
+    it "is not valid with correct email and incorrect password" do
+      subject.save
+      expect(User.authenticate_with_credentials(
+        "kendall.rowe312@gmail.com",
+        "12345" 
+      )).to be_nil
+    end
+  end
 end
